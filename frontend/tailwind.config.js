4f61f7ff58784572c8552b5353217174885c413a
mdatsev <martin.datsev.p@gmail.com>
2022-12-14 02:19:53 +0200

    player interface

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%": { transform: "rotateZ(3deg)" },
+          "2%": { transform: "rotateZ(12deg)" },
+          "4%": { transform: "rotateZ(-8deg)" },
+          "6%": { transform: "rotateZ(8deg)" },
+          "8%": { transform: "rotateZ(-4deg)" },
+          "10%": { transform: "rotateZ(4deg)" },
+
+          "33%": { transform: "rotateZ(-3deg)" },
+          "56%": { transform: "rotateZ(3deg)" },
+          "79%": { transform: "rotateZ(-3deg)" },
+          "100%": { transform: "rotateZ(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 4s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
